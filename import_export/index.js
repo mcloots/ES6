b48ea2c863f925d90a89d97b09f8b48eb7d76e8a
Michaël Cloots <michael.cloots@thomasmore.be>
2022-03-21 22:38:40 +0100

    pad import export classes

---
+++ import_export/index.js
@@ -0,0 +1,15 @@
+//SOLID principles
+//Dependency injection
+//OO
+//Modular
+
+//Use live server!
+
+//We can import exported constants, enumerations, classes
+import { data } from "./example.js";
+
+let updatedData = data;
+
+updatedData.push(5);
+console.log(updatedData);
+
---
+++ padStart_padEnd/index.js
@@ -0,0 +1,8 @@
+//add values to start and end of string
+let example = 'Michaël';
+
+//Adds a in beginning of string to meet 10 characters length
+console.log(example.padStart(10,'a'));
+
+//Adds a at the end of string to meet 10 characters length
+console.log(example.padEnd(10,'a'));
